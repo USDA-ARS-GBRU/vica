ecef1ea4289fcd09f07e3e8eb840f7fb34deaea9
Adam R. Rivers <adam.rivers@ars.usda.gov>
2017-11-29 16:46:25 -0500

    complete testing of split_and_shred.py

--- vica/split_and_shred.py
+++ vica/split_and_shred.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''split_and_shred.py'''
+"""split_and_shred.py"""
 
 import pyfaidx
 import ete3
@@ -7,44 +7,56 @@ import pandas
 import logging
 import numpy
 import subprocess
-
-#workflow
-# Index sorted.fa.gz, the file from bbtools taxonomy processing
-# Create a table of each sequence with:
-#Seqname, length, Taxid, split tax level, training unclassified
-#Claculate the length per class and the length per taxsplit level  and the length per taxid
-
-# capture n contigs from each training class
-# for each class:
-#    assign to a
+import os
 
 #load fasta records and create index
 def _read_data(file):
-    '''read a fasta or bgzf fasta and optionally an equivelantly named faidx and return a pyfaidx handle'''
+    """read a fasta or bgzf fasta and optionally an equivelantly named faidx and return a pyfaidx handle"""
     seqobj = pyfaidx.Fasta(file, read_ahead=10000)#, read_long_names=True)
     return seqobj
 
 
 def _shuffle_keys(ddict):
-    '''take the pyfaidx file and return a suffled list of the keys'''
+    """take the pyfaidx file and return a suffled list of the keys"""
     keylist = []
     for key in ddict.keys():
         keylist.append(key)
     kls = random.shuffle(keylist)
     return keylist
 
-def _get_taxonomy(minlevel, maxlevel, treefile, infile):
-    '''takes a bbtools formatted sequence fasta file andretuns a list containing the lineage at the min and max levels requested'''
-    options = ["taxonomy.sh",
-               "tree=" + treefile,
-               "minlevel=" + minlevel,
-               "maxlevel=" + minlevel,
-               "in=" + infile]
-    sendsketchout = subprocess.run(options, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return sendsketchout.stdout.decode('utf-8').splitlines()
+# def _get_taxonomy(minlevel, maxlevel, treefile, infile):
+#     """takes a bbtools formatted sequence fasta file and retuns a list containing the lineage at the min and max levels requested"""
+#     options = ["taxonomy.sh",
+#                "tree=" + treefile,
+#                "minlevel=" + minlevel,
+#                "maxlevel=" + minlevel,
+#                "in=" + infile]
+#     sendsketchout = subprocess.run(options, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#     logging.info(sendsketchout.stderr.decode('utf-8'))
+#     return sendsketchout.stdout.decode('utf-8').splitlines()
+#
+# def _parse_bbtools_taxonomy(taxlist):
+#     newdict = {}
+#     for item in taxlist:
+#         if item == '':
+#             taxid = None
+#         elif item.startswith("tid"):
+#             readid = item.strip().split(" ")[0]
+#             taxid = readid.split("|")[1]
+#         else:
+#             ll = item.strip().split('\t')
+#             newdict[taxid] = {"level":ll[0], "level_taxid"[ll[1], "levelname":ll[2]}
+#             next
+#     labels = ["seqeuence_taxid","level","level_taxid","levelname"]
+#     df = pandas.DataFrame.from_dict(newlist, columns=labels)
+#     # by default pandas drops all entries after the first
+#     df = df.drop_duplicates("seqeuence_taxid")
+#     return df
 
-def _profile_sequences(seqobj, ncbiobj, minsplitlevel, maxsplitlevel, classes):
-    '''collect data on all sequences in the reference db'''
+
+
+def _profile_sequences(seqobj, ncbiobj, splitlevel, classes):
+    """collect data on all sequences in the reference db"""
     taxlevels = ['species', 'genus', 'family', 'order','class', 'phylum', 'kingdom', ]
     datadict = {}
     for key in seqobj.keys():
@@ -71,24 +83,20 @@ def _profile_sequences(seqobj, ncbiobj, minsplitlevel, maxsplitlevel, classes):
     return df
 
 def _split_levels(testfrac, df, classes):
-    '''Split the taxa at the selected level into test and train'''
+    """Split the taxa at the selected level into test and train"""
     cd = {}
     for taxid in classes:
          dff = df[df.classid==taxid]
-         print(dff)
          classids =set(dff['taxlevelid'])
-         print(classids)
          clength = len(classids)
-         print(clength)
          test = round(clength*testfrac)
-         print(test)
          testids = set(numpy.random.choice(a=list(classids), size = test, replace=False))
          trainids = set(classids) - testids
          cd[taxid]={'test':testids,'train': trainids, 'total':clength}
     return(cd)
 
 def writeseq(record, pos, length, handle):
-    '''writes a fasta sequence to a file, adding position information to the id'''
+    """writes a fasta sequence to a file, adding position information to the id"""
     seqlist = []
     label = (">" + record.name + "|pos|" + str(pos) + ".." + str(pos + length) + "\n")
     end = pos + length
@@ -101,20 +109,22 @@ def writeseq(record, pos, length, handle):
 
 # >>> Left off here<<<
 def _select_contigs(n_per_class, cd, outdir, length, df, seqobj):
-    '''select contigs for testing and training for each classifier class with
+    """select contigs for testing and training for each classifier class with
        even sampling in each taxon at the selected taxonomic level.
-       Writes to the output directory.'''
+       Writes to the output directory."""
 
     def process_examples(exampletype):
         tot_recs = 0
         for taxid in cd:
             recs_written = 0
             #calcualte the samples required to get an even sampling from each taxa level
-            rec_per_level = round(n_per_classes/cd[taxid]['total'])
+            rec_per_level = round(n_per_class/cd[taxid]['total'])
             # select those taxa from the dataframe
-            testdf = df[df.taxlevelid in cd[taxid][exampletype]]
+            testdf = df[df.taxlevelid.isin(cd[taxid][exampletype])]
             # calculate number of samples per taxa level
-            testsize = n_per_class * len(cd[taxid][exampletype])/cd[taxid]['total']
+            print(testdf)
+            testsize = round(n_per_class * len(cd[taxid][exampletype])/cd[taxid]['total'])
+            print(type(testsize))
             # select the examples contigs based in their fraction of genomic length in the taxa level
             testvect = numpy.random.choice(a= testdf.index.values, p=testdf["length"]/sum(testdf["length"]),size=testsize)
             #For each contig select a fragment of desired length
@@ -125,12 +135,12 @@ def _select_contigs(n_per_class, cd, outdir, length, df, seqobj):
                     # verify the contig is long enough
                     if seq_length > length:
                         # select a random starting point
-                        pos = np.random.choice(seq_length - length)
+                        pos = numpy.random.choice(seq_length - length)
                         # calculate an end position
                         endpos = pos + length
                         # verif y the reads is less than 10% N's
-                        if record[pos: seglen + pos].seq.count("N")/seglen < 0.1:
-                            writeseq(record, pos, length, outhandle)
+                        if seqobj[name][pos: endpos].seq.count("N")/length < 0.1:
+                            writeseq(seqobj[name], pos, length, outhandle)
                             recs_written += 1
                             tot_recs += 0
             logging.info("Wrote {} fragmented sequences to the {} directory in the class {}".format(recs_written, exampletype, taxid))
@@ -142,9 +152,11 @@ def _select_contigs(n_per_class, cd, outdir, length, df, seqobj):
         os.makedirs(outdir)
     # make output directories
     testdir = os.path.join(outdir,"test")
-    os.mkdir(testdir)
+    if not os.path.exists(testdir):
+        os.mkdir(testdir)
     traindir = os.path.join(outdir,"train")
-    os.mkdir(traindir)
+    if not os.path.exists(testdir):
+        os.mkdir(traindir)
     # for each test and train:
     testcount = process_examples('test')
     traincount = process_examples('train')
@@ -156,12 +168,10 @@ def shred_all(fastafile, outdir, length=5000, n_per_class=100000, testfrac =0.1,
                        2157: "Archaea",
                        2759: "Eukaryota",
                        10239: "Viruses"}):
-    '''shred all sequences to the desired length'''
+    """shred all sequences to the desired length"""
     # Read data as pyfaidx object
     seqobj = _read_data(fastafile)
     ncbi = ete3.NCBITaxa()
-    df = _profile_sequences(seqobj, ncbi, 'family', classes)
+    df = _profile_sequences(seqobj, ncbi, splitlevel, classes)
     cd = _split_levels(testfrac=testfrac, df=df, classes=classes)
     _select_contigs(n_per_class=n_per_class, cd=cd, outdir=outdir,length=length,df=df, seqobj=seqobj)
-
-    classes={2: "Bacteria", 2157: "Archaea", 2759: "Eukaryota",10239: "Viruses"}
