cd7f6926d1c8696bb5f86c85c780554e79fef5d8
Adam R. Rivers <adam.rivers@ars.usda.gov>
2017-12-05 15:15:08 -0500

    fixed missing commas in setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     classifiers=['Topic :: Scientific/Engineering :: Bio-Informatics',
                  'Topic :: Scientific/Engineering :: Artificial Intelligence',
                  'Programming Language :: Python :: 3.5',
-                 'Development Status :: 2 - Pre-Alpha']
+                 'Development Status :: 2 - Pre-Alpha'],
     keywords='virus classifier metagenome RNA DNA microbiome tensorflow',
-    url='http://github.com/usda-ars-gbru/vica'
+    url='http://github.com/usda-ars-gbru/vica',
     test_suite ='nose.collector',
     #tests_require=['nose'],
     author='Adam R. Rivers, Qingpeng Zhang',
@@ -19,5 +19,5 @@ setup(
     install_requires=['tensorflow>=1.4', 'pandas>-0.20.3', 'numpy>=1.13.1', 'biopython>=1.70','scipy>=0.19.0' 'khmer>=2.1.1','ete3', 'pyfaidx>=0.5', 'pyyaml'],
     tests_require=['nose'],
     include_package_data=True,
-    scripts=['vica/vica-cli.py']
+    scripts=['vica/vica-cli.py'],
     zip_safe=False)
