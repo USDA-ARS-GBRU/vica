ddf37c1ebcbcbe898582807bafc251546ff43367
Adam R. Rivers <adam.rivers@ars.usda.gov>
2017-12-19 15:09:36 -0500

    changes to fix pip install

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vica',
-    version='0.1.3',
+    version='0.1.4',
     packages=['vica'],
     license='Other/Proprietary License',
     description="find highly divergent DNA and RNA viruses in microbiomes",
