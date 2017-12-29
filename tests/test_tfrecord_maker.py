314ac1ba5175bd77011e4b844c93c2e7ce6c0b26
Adam R. Rivers <adam.rivers@ars.usda.gov>
2017-12-29 10:17:58 -0500

    Completed TFrecord maker tests

--- tests/test_tfrecord_maker.py
+++ tests/test_tfrecord_maker.py
@@ -31,9 +31,32 @@ def test_count_features():
                                        j3="tests/test-data/join_3.csv")
     eq_(outdict, {"j1":2, "j2":3, "j3":3})
 
-# def test_csv_to_tfrecords():
-#     vica.tfrecord_maker._csv_to_tfrecords(kmerfile=,
-#         codonfile=,
-#         minhashfile=,
-#         mergefile=
-#         tfrecordfile=)
+### Stopping point
+def test_csv_to_tfrecords():
+    td = tempfile.mkdtemp()
+    print(td)
+    outfile1 = os.path.join(td, "2testseqs.tfrecord")
+    vica.tfrecord_maker._csv_to_tfrecords(kmerfile="tests/test-data/5mers.csv",
+        codonfile="tests/test-data/codons.csv",
+        minhashfile="tests/test-data/minhashcsvtest.csv",
+        mergefile="tests/test-data/mergefile.csv",
+        tfrecordfile=outfile1,
+        label=0)
+    # ordering of fields in TFrecords is stochastic so just compare size
+    refsize = os.stat("tests/test-data/2testseqs.tfrecord").st_size
+    eq_(refsize, os.stat(outfile1).st_size)
+
+def test_convert_to_tfrecords():
+    td = tempfile.mkdtemp()
+    print(td)
+    outfile1 = os.path.join(td, "2testseqs.tfrecord")
+    vica.tfrecord_maker.convert_to_tfrecords(dtemp= td,
+        kmerfile="tests/test-data/5mers.csv",
+        codonfile="tests/test-data/codons.csv",
+        minhashfile="tests/test-data/minhashcsvtest.csv",
+        tfrecordfile=outfile1,
+        label=0,
+        sort=True)
+    # ordering of fields in TFrecords is stochastic so just compare size
+    refsize = os.stat("tests/test-data/2testseqs.tfrecord").st_size
+    eq_(refsize, os.stat(outfile1).st_size)
