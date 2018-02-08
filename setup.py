4ba8e2323329c5fb85e2a741bdf55d62bb04cc4e
Adam R. Rivers <adam.rivers@ars.usda.gov>
2018-02-08 15:38:06 -0500

    updated pandas for travis ci

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     #tests_require=['nose'],
     author='Adam R. Rivers, Qingpeng Zhang',
     author_email='adam.rivers@ars.usda.gov',
-    install_requires=['tensorflow>=1.4', 'pandas>=0.20.3', 'numpy>=1.13',
+    install_requires=['tensorflow>=1.4', 'pandas>=0.20.3', 'numpy>=1.14',
         'biopython>=1.70','scipy>=0.19.0', 'khmer>=2.1.1','ete3',
         'pyfaidx>=0.5', 'pyyaml', 'scikit-learn>=0.19.0'],
     python_requires='>3.5',
