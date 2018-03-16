e1ecd001638de72383796cd44111cd2d6a9f8669
Adam R. Rivers <adam.rivers@ars.usda.gov>
2018-03-16 11:16:44 -0700

    version bump to correspond with pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vica',
-    version='0.1.4',
+    version='0.1.5',
     packages=['vica'],
     license='License :: OSI Approved :: BSD License',
     description="find highly divergent DNA and RNA viruses in microbiomes",
