493f8a980d9fd93e9fe2226363ce0c4de056a36b
Adam R. Rivers <adam.rivers@ars.usda.gov>
2019-03-14 13:17:10 -0400

    changed hmmstats to hmmstat

--- vica/hmmer.py
+++ vica/hmmer.py
@@ -66,6 +66,8 @@ def _get_tokens(hmmfile):
     sendsketchout = subprocess.run(options, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     namelist = []
     for line in sendsketchout.stdout.decode('utf-8'):
+        if not line.strip(): # remove blank lines
+            continue
         if not line.startswith('#') and not line.startswith(" "):
             linelist = line.strip().split()
             namelist.append(linelist[1])
