81da5bfc4b7c7652c5c3b4a0d0271fd8d0d44792
Adam R. Rivers <adam.rivers@ars.usda.gov>
2019-03-25 12:03:01 -0400

    adding auc metric

--- vica/train_eval2.py
+++ vica/train_eval2.py
@@ -203,7 +203,7 @@ def train_and_eval(train_files, eval_files, modeldir, configpath=vica.CONFIG_PAT
         filenames=eval_files)
     #def my_auc(labels, predictions):
     #    return {'auc': tf.metrics.auc(labels, predictions['probabilities'], curve="PR")}
-    my_estimator = create_log_estimator(modeldir=modeldir, n_classes=n_classes)
+    my_estimator = create_DNN_estimator(modeldir=modeldir, n_classes=n_classes)
     #my_estimator = tf.estimator.add_metrics(my_estimator, my_auc)
     train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn)
     eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn)
